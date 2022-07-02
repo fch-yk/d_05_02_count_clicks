da12f8ff79cff7c47064048df37551f466e26b04
yk <yefim_korshever@mail.ru>
2022-07-02 22:45:41 +0300

    Print shortened link

--- clicks.py
+++ clicks.py
@@ -7,16 +7,12 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     bitly_token = os.getenv('bitly_token')
-
-    url = 'https://api-ssl.bitly.com/v4/user'
-
-    headers = {
-        'Authorization': f'Bearer {bitly_token}',
-    }
-
-    response = requests.get(url, headers=headers)
+    url = 'https://api-ssl.bitly.com/v4/bitlinks/'
+    headers = {'Authorization': f'Bearer {bitly_token}'}
+    payload = {'long_url': 'https://www.youtube.com/'}
+    response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
-    print(response.text)
+    print(response.json()['link'])
 
 
 if __name__ == '__main__':
