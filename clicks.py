62299c085019ba93707121f3e107b5eca0ef28fd
yk <yefim_korshever@mail.ru>
2022-07-03 15:59:50 +0300

    Check if link is bitlink

--- clicks.py
+++ clicks.py
@@ -6,6 +6,17 @@ import requests
 from dotenv import load_dotenv
 
 
+def is_bitlink(link, token):
+    parsed_link = urlparse(link)
+    bitlink = f'{parsed_link.hostname}{parsed_link.path}'
+    url = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'
+    url = url.format(bitlink=bitlink)
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.ok
+
+
 def count_clicks(token, link):
     parsed_link = urlparse(link)
     bitlink = f'{parsed_link.hostname}{parsed_link.path}'
@@ -31,20 +42,29 @@ def main():
     token = os.getenv('bitly_token')
     user_input = input('Input link: ')
     try:
-        bitlink = shorten_link(token, user_input)
-    except requests.exceptions.HTTPError as error:
-        print(f'Incorrect link. Error: {error}', file=sys.stderr)
-        sys.exit()
+        link_is_bitlink = is_bitlink(user_input, token)
+    except requests.exceptions.HTTPError:
+        link_is_bitlink = False
 
-    print('Битлинк', bitlink)
-
-    try:
-        total_clicks = count_clicks(token, bitlink)
-    except requests.exceptions.HTTPError as error:
-        print(f'Anable to count total clicks. Error: {error}', file=sys.stderr)
-        sys.exit()
-
-    print('Total clicks', total_clicks)
+    if link_is_bitlink:
+        try:
+            total_clicks = count_clicks(token, user_input)
+        except requests.exceptions.HTTPError as error:
+            print(
+                f'Anable to count total clicks. Error: {error}',
+                file=sys.stderr
+            )
+            sys.exit()
+        else:
+            print('Total clicks: ', total_clicks)
+    else:
+        try:
+            bitlink = shorten_link(token, user_input)
+        except requests.exceptions.HTTPError as error:
+            print(f'Incorrect link. Error: {error}', file=sys.stderr)
+            sys.exit()
+        else:
+            print('Bitlink: ', bitlink)
 
 
 if __name__ == '__main__':
