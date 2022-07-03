9ba1a56173e33402379bb37d947f2804fd31906d
yk <yefim_korshever@mail.ru>
2022-07-03 10:20:20 +0300

    Add shorten_link function

--- clicks.py
+++ clicks.py
@@ -4,15 +4,20 @@ import requests
 from dotenv import load_dotenv
 
 
-def main():
-    load_dotenv()
-    bitly_token = os.getenv('bitly_token')
+def shorten_link(token, link):
     url = 'https://api-ssl.bitly.com/v4/bitlinks/'
-    headers = {'Authorization': f'Bearer {bitly_token}'}
-    payload = {'long_url': 'https://www.youtube.com/'}
+    headers = {'Authorization': f'Bearer {token}'}
+    payload = {'long_url': link}
     response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
-    print(response.json()['link'])
+    return response.json()['link']
+
+
+def main():
+    load_dotenv()
+    token = os.getenv('bitly_token')
+    link = 'https://www.youtube.com/'
+    print('Битлинк', shorten_link(token, link))
 
 
 if __name__ == '__main__':
