6098db552337092d0d602de4bc6d0e69e854ecaf
yk <yefim_korshever@mail.ru>
2022-07-03 15:12:08 +0300

    Count clicks

--- clicks.py
+++ clicks.py
@@ -1,8 +1,20 @@
 import os
 import sys
+from urllib.parse import urlparse
 
 import requests
 from dotenv import load_dotenv
+
+
+def count_clicks(token, link):
+    parsed_link = urlparse(link)
+    bitlink = f'{parsed_link.hostname}{parsed_link.path}'
+    url = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    url = url.format(bitlink=bitlink)
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()['total_clicks']
 
 
 def shorten_link(token, link):
@@ -17,14 +29,22 @@ def shorten_link(token, link):
 def main():
     load_dotenv()
     token = os.getenv('bitly_token')
-    link = input('Input link: ')
+    user_input = input('Input link: ')
     try:
-        bitlink = shorten_link(token, link)
+        bitlink = shorten_link(token, user_input)
     except requests.exceptions.HTTPError as error:
         print(f'Incorrect link. Error: {error}', file=sys.stderr)
         sys.exit()
 
     print('Битлинк', bitlink)
+
+    try:
+        total_clicks = count_clicks(token, bitlink)
+    except requests.exceptions.HTTPError as error:
+        print(f'Anable to count total clicks. Error: {error}', file=sys.stderr)
+        sys.exit()
+
+    print('Total clicks', total_clicks)
 
 
 if __name__ == '__main__':
